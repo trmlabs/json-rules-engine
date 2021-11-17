e10400adaf1c2d2b02ddd865d7cd95744df45fd4
Rahul Raina <rahulr0609@hotmail.com>
2021-11-17 11:44:57 -0500

    Updated webpack to sub settimeout and add content hashing to file name

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,19 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     library: 'jre',
     libraryTarget: 'var'
   },
   plugins: [
+    // Needed to bypass the setTimeout
+    // used by EventEmitter
+    // Since we do NOT actually pass in a non-zero
+    // and we do not need things to execute on a separate thread
+    // we can just execute the method immediately
+    new webpack.DefinePlugin({
+      setTimeout: '(fn => fn())',
+    }),
     new webpack.EnvironmentPlugin({
       DEBUG: false,
     })
