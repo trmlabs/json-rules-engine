dd36cb21975c8156ba76b16634d179db0e0c1820
Rahul <1559764+rraina@users.noreply.github.com>
2021-11-19 08:52:25 -0500

    Fix bundle to work with latest JRE (#2)
    * Updated webpack to sub settimeout and add content hashing to file name
    
    * Added git hash to webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
 const path = require('path');
 const webpack = require('webpack');
 
+let commitHash = require('child_process')
+  .execSync('git rev-parse --short HEAD')
+  .toString()
+  .replace(/(\r\n|\n|\r)/gm, "");
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: `bundle.${commitHash}.[contenthash].js`,
     library: 'jre',
     libraryTarget: 'var'
   },
   plugins: [
+    // Needed to bypass the setTimeout
+    // used by EventEmitter
+    // Since we do NOT actually pass in a non-zero
+    // and we do not need things to execute on a separate thread
+    // we can just execute the method immediately
+    new webpack.DefinePlugin({
+      setTimeout: '(fn => fn())',
+    }),
     new webpack.EnvironmentPlugin({
       DEBUG: false,
     })
